f9b8975f8d0c8ff337d4bbed2cef4ee0615b1394
AlexSorx <sorore@ualberta.ca>
2023-02-10 13:32:26 -0700

    tests pass license updated

--- httpclient.py
+++ httpclient.py
@@ -32,8 +32,12 @@ class HTTPResponse(object):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
     #def get_host_port(self,url):
+
+    def __init__(self):
+        self.port = 80
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,13 +45,24 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        #Status code should be the integer following HTTP/1.1.
+        return int(data.split("\r\n")[0].split(" ")[1])
 
     def get_headers(self,data):
-        return None
+        #find the start index of the body
+        body_start = data.find("\r\n\r\n") 
+        #find index of empty line
+        empty_line = data.find("\r\n") 
+        #Header is the body to the empty line
+        headers = data[body_start:empty_line] 
+        return headers
 
     def get_body(self, data):
-        return None
+        #find index of start of body
+        body_start = data.find("\r\n\r\n") 
+        #Splice response accordingly
+        body = data[body_start:] 
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,15 +82,75 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def url_parser(self, url):
+        
+        #Use urllib to parse url 
+        parsedURL = urllib.parse.urlparse(url)
+        #get path if not path return  index
+        path = parsedURL.path
+        if not path:
+           path = "/"
+        #get port if not port return 80
+        port = parsedURL.port
+        if not port:
+            port =  80
+        #get host if not host return path and index 
+        hostname = parsedURL.hostname
+        if not hostname:
+            hostname = parsedURL.path
+            path = "/"
+        #Store parsed elements in dict and return dict
+        parsed_elements = {"path" : path, "port": port, "hostname" : hostname}
+        return parsed_elements          
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        #fetch parsed url
+        parsed_elements = self.url_parser(url)
+        #connect on port with host
+        self.connect(parsed_elements["hostname"], parsed_elements["port"])
+        #base request 
+        request = f'GET {parsed_elements["path"]} HTTP/1.0\r\nHost: {parsed_elements["hostname"]}:{parsed_elements["port"]}\r\nConnection: Close\r\n\r\n'
+        self.sendall(request)
+        receivedMessage = self.recvall(self.socket)
+        self.close()
+
+        code = self.get_code(receivedMessage)
+        body = self.get_body(receivedMessage)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        #fetch parsed url
+        parsed_elements = self.url_parser(url)
+        #connect to port on host
+        self.connect(parsed_elements["hostname"], parsed_elements["port"])
+        #base post request
+        request = f'POST {parsed_elements["path"]} HTTP/1.0\r\nHost: {parsed_elements["hostname"]}:{parsed_elements["port"]}\r\nConnection: Close\r\n'
+        #if the request has arguments find the arguments 
+        if args:
+            foundArgs = self.find_args(args)
+            request += "Content-Type: application/x-www-form-urlencoded\r\n"
+            request += f'Content-Length: {len(foundArgs.encode("utf-8"))}\r\n\r\n'
+            request += foundArgs + '\r\n'
+        #if no arguments return base request  
+        elif args == None:
+            request += "Content-Length: 0\r\n"
+        request += '\r\n'
+        self.sendall(request)
+        receivedMessage = self.recvall(self.socket)
+        self.close()
+
+        code = self.get_code(receivedMessage)
+        body = self.get_body(receivedMessage)
         return HTTPResponse(code, body)
+
+    #find_args method finds arguments and stores them accordingly
+    def find_args(self, args):
+        foundArgs = ""
+        for key in args:
+            val = args[key]
+            foundArgs += key + '=' + val + '&'
+        return foundArgs[0:len(foundArgs)-1]
+
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
